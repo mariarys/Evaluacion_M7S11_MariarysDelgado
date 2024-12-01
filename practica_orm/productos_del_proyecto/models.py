0af3d5ce2e5d97cc818549427d353a5479c6edb4
Mariarys <delgadomariarys@gmail.com>
2024-12-01 15:52:03 -0300

    Drilling M7S6

--- practica_orm/productos_del_proyecto/models.py
+++ practica_orm/productos_del_proyecto/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class Fabricante(models.Model):
     nombre = models.CharField(max_length=100, unique = True)
+    pais = models.CharField(max_length = 100)
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     descripcion = models.TextField()
+    f_vencimiento = models.DateField(null = True, blank = True)
     fabricante = models.ForeignKey (Fabricante, on_delete=models.CASCADE, related_name='productos',null=True )
 
     def __str__(self):
