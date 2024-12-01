6f343a8fa67b75801ea3b7bf81a83da898cbfb6e
Mariarys <delgadomariarys@gmail.com>
2024-12-01 16:51:48 -0300

    rebound M7S7

--- practica_orm/productos_del_proyecto/admin.py
+++ practica_orm/productos_del_proyecto/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Fabricante,Producto
 
+class FabricanteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'pais') 
+    search_fields = ('nombre', 'pais') 
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'precio', 'fabricante')
+    list_filter = ('fabricante',)  
+    search_fields = ('nombre', 'descripcion') 
+    ordering = ('fabricante', 'nombre')  
+
+admin.site.register(Fabricante,FabricanteAdmin)
+admin.site.register(Producto,ProductoAdmin)
 # Register your models here.
--- practica_orm/productos_del_proyecto/models.py
+++ practica_orm/productos_del_proyecto/models.py
@@ -4,6 +4,9 @@ class Fabricante(models.Model):
     nombre = models.CharField(max_length=100, unique = True)
     pais = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return f"{self.nombre}"
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
@@ -12,4 +15,4 @@ class Producto(models.Model):
     fabricante = models.ForeignKey (Fabricante, on_delete=models.CASCADE, related_name='productos',null=True )
 
     def __str__(self):
-        return f"{self.nombre} - {self.fabricante.nombre}"
+        return f"{self.nombre}"
