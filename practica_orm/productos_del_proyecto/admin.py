f7782c743fc0f4e03b1f447b9afe733b049a1e2f
Mariarys <delgadomariarys@gmail.com>
2024-12-08 09:49:56 -0300

    Rebound M7S11

--- practica_orm/productos_del_proyecto/admin.py
+++ practica_orm/productos_del_proyecto/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
-from .models import Fabricante,Producto
+from .models import Fabricante, Producto
 
 class FabricanteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'pais') 
-    search_fields = ('nombre', 'pais') 
+    list_display = ('id', 'nombre', 'pais')  
+    search_fields = ('nombre', 'pais')  
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion', 'precio', 'fabricante')
-    list_filter = ('fabricante',)  
-    search_fields = ('nombre', 'descripcion') 
-    ordering = ('fabricante', 'nombre')  
+    list_display = ('id', 'nombre', 'descripcion', 'costo','f_vencimiento', 'fabricante')
+    list_filter = ('nombre','fabricante',)
+    search_fields = ('nombre', 'descripcion')
+    ordering = ('nombre',)
+    list_per_page = 2 
 
-admin.site.register(Fabricante,FabricanteAdmin)
-admin.site.register(Producto,ProductoAdmin)
+    def costo(self, obj):
+        if obj. precio < 3000:
+            return "Costo Bajo"
+        else:
+            return "Costo Alto"
+      
+    
+    costo.short_description = 'costo'
+    costo.admin_order_field = ' precio' 
+
+admin.site.register(Fabricante, FabricanteAdmin)
+admin.site.register(Producto, ProductoAdmin)
 # Register your models here.
